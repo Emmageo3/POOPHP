62c0780dbb091265ac6fb205abe59fc9ebe65af7
Emmageo3 <kanfanyemma22@gmail.com>
2022-02-11 17:50:06 +0000

    ok

--- Person.php
+++ Person.php
@@ -1,21 +1,28 @@
+
 <?php
 
     //creer une classe personne  
     class Person
     {
-        var $firstName = 'Emma';
-        var $lastName = 'Kanfany';
-        var $age = '20';
+        var $firstName;
+        var $lastName;
+        var $age;
 
-        public function __construct(){
-            echo 'Je suis le constructeur';
+        public function __construct($firstName, $lastName, $age){
+            $this->firstname = $firstName;
+            $this->lastname = $lastName;
+            $this->age = $age;
+        }
+
+        public function fullName(){
+            echo $this->firstname . ' ' . $this->lastname;
         }
     }
 
     //instancier la classe 
-    $emma = new Person;
-
-    echo $emma->lastName;
+    $emma = new Person('Emma', 'Kanfany', 21);
+    $seyna = new Person('Seynabou', 'Dione', 22);
+    $seyna->fullName();
 
 
 ?>
