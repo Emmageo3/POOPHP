1b7826832d3ef95add8f9b46ff272799a58b78dd
Emmageo3 <kanfanyemma22@gmail.com>
2022-02-15 12:50:29 +0000

    hydrater

--- Personnage.php
+++ Personnage.php
@@ -63,6 +63,21 @@
             }
         }
 
+        public function hydrate(array $donnees){
+            foreach($donnees as $key => $value){
+                $method = 'set'.ucfirst($key);
+      
+                if (method_exists($this, $method))
+                {
+                    $this->$method($value);
+                }
+            }
+        }
+
+        public function __construct(array $donnees) {
+            $this->hydrate($donnees);
+        }
+
     }
 
 ?>
